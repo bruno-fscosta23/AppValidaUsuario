d67225dd3177310c9d00dd602b1c0ca6de70cb30
bruno.fscosta <brunofsc_@hotmail.com>
2019-08-13 13:43:15 -0300

    novo projeto hoje

---
+++ app/src/main/java/br/sp/senac/appvalidausuario/DescPersonagem_Activity.java
@@ -0,0 +1,14 @@
+package br.sp.senac.appvalidausuario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DescPersonagem_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.desc_personagem_layout);
+    }
+}
